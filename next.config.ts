0ffaad0106589884e3f69d20a6ab1a9b42fded07
Liel Malihi <liel.malihi@Liels-MacBook-Pro.local>
2025-09-23 19:43:35 +0300

    try

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from 'next';
 const config: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: '/seats-app',
-  assetPrefix: '/seats-app/',
 };
 
 export default config;
