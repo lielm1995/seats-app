9c70589fa98a48efb03143dc1b5672e0b1c564f3
Liel Malihi <liel.malihi@Liels-MacBook-Pro.local>
2025-09-24 15:33:16 +0300

    fix

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from 'next';
-
-export default {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: '/seats-app',
-  assetPrefix: '/seats-app/',
-} satisfies NextConfig;
